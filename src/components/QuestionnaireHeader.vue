e751b6ff52d81845ed636e0d62e4dc0f0889c149
Yutthana Siphuengchai <yutthana.s@outlook.com>
2019-05-16 15:35:19 +0700

    Create QuestionnaireHeader component.

---
+++ src/components/QuestionnaireHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <button type="button"
+            @click="onSubmit"
+            :disabled="!enableSubmitButton"
+    >
+      {{ submitButtonText }}
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'QuestionnaireHeader',
+  props: {
+    enableSubmitButton: Boolean,
+    onSubmit: Function,
+    submitButtonText: String,
+    title: String,
+  }
+}
+</script>
